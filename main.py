eaf94a3c8fa623006352f3efb7cc0b8f858d4f9a
exerussus <solovyov.production@gmail.com>
2022-06-20 12:16:43 +0300

    Created JsonOperations class. Created read and save methods of JsonOperations class.

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+class JsonOperations:
+
+    @staticmethod
+    def read(name, ref='data/'):
+        """Читает JSON файл.\n
+        Аргументы:\n
+        ref - путь к файлу (по умолчанию data/), \n
+        name - имя файла без указания формата."""
+        from json import load
+        with open(ref + name + '.json') as json_import_data:
+            data_file = load(json_import_data)
+        return data_file
+
+    @staticmethod
+    def save(name, file, ref='data/'):
+        """Сохраняет значение переменной в JSON файл.\n
+        Аргументы: \n
+        ref - ссылка на файл, \n
+        name - имя файла без указания формата, \n
+        data_file - переменная,
+        значение которой сохраняется в JSON файл."""
+
+        from json import dump
+        with open(ref + name + '.json', 'w') as add_info_file:
+            dump(file, add_info_file)
+
