02bb66bae854fc1f21d1ddda64022c3a4ae55d91
exerussus <solovyov.production@gmail.com>
2022-08-23 12:13:11 +0300

    Minor refracting

--- main.py
+++ main.py
@@ -3,26 +3,26 @@
 class JsonOperations:
 
     @staticmethod
-    def read(name, ref='data/'):
-        """Читает JSON файл.\n
-        Аргументы:\n
-        ref - путь к файлу (по умолчанию data/), \n
+    def read(name, path=''):
+        """Возвращает значение JSON файла.
+        Аргументы:
+        path - путь каталога с JSON файлом (по умолчанию path=''),
         name - имя файла без указания формата."""
         from json import load
-        with open(ref + name + '.json') as json_import_data:
+        with open(path + name + '.json') as json_import_data:
             data_file = load(json_import_data)
         return data_file
 
     @staticmethod
-    def save(name, file, ref='data/'):
-        """Сохраняет значение переменной в JSON файл.\n
-        Аргументы: \n
-        ref - ссылка на файл, \n
-        name - имя файла без указания формата, \n
+    def save(name, file, path=''):
+        """Сохраняет значение переменной в JSON файл.
+        Аргументы: 
+        path - путь каталога где будет сохранён JSON вайл, 
+        name - имя файла без указания формата, 
         file - переменная,
         значение которой сохраняется в JSON файл."""
 
         from json import dump
-        with open(ref + name + '.json', 'w') as add_info_file:
+        with open(path + name + '.json', 'w') as add_info_file:
             dump(file, add_info_file)
 
